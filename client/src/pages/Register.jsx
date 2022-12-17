544ee70f1f82cb94da17fe010f92fe06e449c3f2
Jabame <justinetaglucop@gmail.com>
2022-12-18 05:08:29 +0800

    Fix Register to Populate data  in the Database

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,8 +1,73 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+import { Slide, toast } from 'react-toastify';
+
+import { register, reset } from '../redux/auth/authSlice';
+
+import Loading from '../components/Loading';
+
 const Register = () => {
+  const [registerData, setRegisteredData] = useState({
+    email: '',
+    username: '',
+    password: '',
+    cpassword: '',
+    image: '',
+  });
+
+  const { email, username, password, cpassword, image } = registerData;
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  );
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message, {
+        transition: Slide,
+        theme: 'colored',
+      });
+    }
+
+    if (isSuccess) {
+      toast.success('You are Registered!');
+      navigate('/login');
+    }
+
+    dispatch(reset());
+  }, [user, isError, isSuccess, message, navigate, dispatch]);
+
+  const handleChange = (e) => {
+    setRegisteredData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+
+    const userData = {
+      email,
+      username,
+      password,
+      image,
+    };
+
+    dispatch(register(userData));
+  };
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
     <>
       <div className='max-w-screen-xl px-4 py-16 mx-auto sm:px-6 lg:px-8'>
@@ -20,6 +85,8 @@ const Register = () => {
                   name='email'
                   className='relative block w-full appearance-none p-3  rounded-lg border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm  '
                   placeholder='Enter email'
+                  value={email}
+                  onChange={handleChange}
                 />
               </div>
             </div>
@@ -27,11 +94,13 @@ const Register = () => {
               <label className='text-sm font-medium'>Username</label>
               <div className='relative mt-1'>
                 <input
-                  type='username'
+                  type='text'
                   id='username'
                   name='username'
                   className='relative block w-full appearance-none p-3  rounded-lg border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm  '
                   placeholder='Enter username'
+                  value={username}
+                  onChange={handleChange}
                 />
               </div>
             </div>
@@ -44,6 +113,8 @@ const Register = () => {
                   name='password'
                   className='relative block w-full appearance-none p-3  rounded-lg border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm'
                   placeholder='Enter password'
+                  value={password}
+                  onChange={handleChange}
                 />
               </div>
             </div>
@@ -56,6 +127,8 @@ const Register = () => {
                   name='cpassword'
                   className='relative block w-full appearance-none p-3  rounded-lg border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm'
                   placeholder='Confirm password'
+                  value={cpassword}
+                  onChange={handleChange}
                 />
               </div>
             </div>
@@ -64,20 +137,23 @@ const Register = () => {
               <div className='relative mt-1'>
                 <input
                   type='file'
-                  id='fileUpload'
-                  name='fileUpload'
+                  id='image'
+                  name='image'
+                  value={image}
+                  onChange={handleChange}
                   className='relative block w-full appearance-none p-3  rounded-lg border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm'
                 />
               </div>
             </div>
-            <Link to='/login'>
-              <button
-                type='submit'
-                className='block w-full px-5 py-3 text-sm font-medium text-white bg-indigo-600 rounded-lg mt-5 '
-              >
-                Register
-              </button>
-            </Link>
+
+            <button
+              type='submit'
+              className='block w-full px-5 py-3 text-sm font-medium text-white bg-indigo-600 rounded-lg mt-5 '
+              onClick={handleRegister}
+            >
+              Register
+            </button>
+
             <div className='flex justify-between'>
               <p className='text-xs sm:text-sm text-center text-gray-500 cursor-pointer'>
                 <span className='undeline'>Already Have an account?</span>
