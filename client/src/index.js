37e547d921bc4815f86b14593307da73629d76d9
Jabame <justinetaglucop@gmail.com>
2022-12-18 05:07:21 +0800

    Modified to add redux, react-router-dom, react-toastify

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,22 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
+//redux
+import { store } from './redux/store/store';
+import { Provider } from 'react-redux';
+
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+      <ToastContainer />
+    </Provider>
   </React.StrictMode>
 );
