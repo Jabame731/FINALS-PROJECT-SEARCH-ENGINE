c266602a5df2c9e133cfb2a364d755f5a524b658
Jabame <justinetaglucop@gmail.com>
2022-12-29 14:24:42 +0800

    FINALS ITME-3 Added a new Route Method
    *  Added a new Route which is WebResults, NewsResults and ImageResults
       in the App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,10 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 
 import { Routes, Route } from 'react-router-dom';
+import NewsResults from './pages/NewsResults';
+import ImageResults from './pages/ImageResults';
+import WebResults from './pages/WebResults';
+import Error from './components/Error';
 
 function App() {
   return (
@@ -10,9 +14,12 @@ function App() {
       <div>
         <Routes>
           <Route path='/' element={<Home />} />
-
           <Route path='login' element={<Login />} />
           <Route path='register' element={<Register />} />
+          <Route path='web-search/:searchTerm' element={<WebResults />} />
+          <Route path='news-search/:searchTerm' element={<NewsResults />} />
+          <Route path='image-search/:searchTerm' element={<ImageResults />} />
+          <Route path='*' element={<Error />} />
         </Routes>
       </div>
     </div>
