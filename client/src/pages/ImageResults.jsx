eeca8df06b93cc9ab335809c64ae2c1508f953aa
Jabame <justinetaglucop@gmail.com>
2022-12-29 14:44:30 +0800

    FINALS ITME-3 Added a new Image and News Results page
    *  Initial file for the Image and News Results page

---
+++ client/src/pages/ImageResults.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ImageResults = () => {
+  return <div>ImageResults</div>;
+};
+
+export default ImageResults;
---
+++ client/src/pages/NewsResults.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NewsResults = () => {
+  return <div>NewsResults</div>;
+};
+
+export default NewsResults;
