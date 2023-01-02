911443364d92b82654252db2f73b06609bb580b1
Jabame <justinetaglucop@gmail.com>
2023-01-02 22:07:42 +0800

    FINALS ITME-3 Added A Route Link to the Homepage
    *  Added a route link to homepage if the URL is not available on the
       application

--- client/src/components/Error.jsx
+++ client/src/components/Error.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Error = () => {
   return (
@@ -15,9 +16,11 @@ const Error = () => {
               homepage.
             </p>
 
-            <button className='px-4 inline py-2 text-sm font-medium leading-5 shadow text-white transition-colors duration-150 border border-transparent rounded-lg focus:outline-none focus:shadow-outline-blue bg-blue-600 active:bg-blue-600 hover:bg-blue-700'>
-              back to homepage
-            </button>
+            <Link to='/'>
+              <button className='px-4 inline py-2 text-sm font-medium leading-5 shadow text-white transition-colors duration-150 border border-transparent rounded-lg focus:outline-none focus:shadow-outline-blue bg-blue-600 active:bg-blue-600 hover:bg-blue-700'>
+                back to homepage
+              </button>
+            </Link>
           </div>
           <div className='max-w-lg'></div>
         </div>
